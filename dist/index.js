0c5789cd5c7c8835696347197882a305a324c4fe
Jason Haslow <jason.haslow@ui.com>
2022-03-28 10:54:28 -0400

    adding logging.

--- dist/index.js
+++ dist/index.js
@@ -8476,6 +8476,7 @@ async function run(){
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
+    console.log('octokit', JSON.stringify(octokit));
 
     const { context = {} } = github;
     const { pull_request } = context.payload;
